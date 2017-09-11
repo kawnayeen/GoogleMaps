ca89e6d9f46b4bdcb0cd0201e1cba1b7fa9d907b
kawnayeen <kawnayeen.08@gmail.com>
2017-09-11 14:01:33 +0600

    initial layout developed

--- MapTypes/app/src/main/java/com/kawnayeen/maptypes/MapsActivity.java
+++ MapTypes/app/src/main/java/com/kawnayeen/maptypes/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -37,10 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng cefaloDhaka = new LatLng(23.752629, 90.375684);
+        CameraPosition target = CameraPosition.builder().target(cefaloDhaka).zoom(13).build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
