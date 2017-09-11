b1ae41ac993d146d9cfa00df5e37ddb0d9f3139c
kawnayeen <kawnayeen.08@gmail.com>
2017-09-11 14:22:28 +0600

    selecting different map types

--- MapTypes/app/src/main/java/com/kawnayeen/maptypes/MapsActivity.java
+++ MapTypes/app/src/main/java/com/kawnayeen/maptypes/MapsActivity.java
@@ -1,7 +1,8 @@
 package com.kawnayeen.maptypes;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,35 +10,43 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button mapBtn;
+    Button satelliteBtn;
+    Button hybridBtn;
+    boolean isMapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapBtn = findViewById(R.id.mapBtn);
+        satelliteBtn = findViewById(R.id.satelliteBtn);
+        hybridBtn = findViewById(R.id.hybridBtn);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mapBtn.setOnClickListener(v -> {
+            if (isMapReady)
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        });
+        satelliteBtn.setOnClickListener(v -> {
+            if (isMapReady)
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        });
+        hybridBtn.setOnClickListener(v -> {
+            if (isMapReady)
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        isMapReady = true;
         LatLng cefaloDhaka = new LatLng(23.752629, 90.375684);
         CameraPosition target = CameraPosition.builder().target(cefaloDhaka).zoom(13).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
