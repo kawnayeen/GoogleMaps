69bc2cc808c73279dc62fbb699df13d8d6bef4df
kawnayeen <kawnayeen.08@gmail.com>
2017-09-15 15:44:29 +0600

    Super simple address selection from map implemented

--- GetAddress/app/src/main/java/com/kawnayeen/getaddress/MainActivity.java
+++ GetAddress/app/src/main/java/com/kawnayeen/getaddress/MainActivity.java
@@ -1,13 +1,62 @@
 package com.kawnayeen.getaddress;
 
-import android.support.v7.app.AppCompatActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    Marker marker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        MarkerOptions selectedLocation = new MarkerOptions().position(googleMap.getCameraPosition().target).title("Selected Location");
+        marker = googleMap.addMarker(selectedLocation);
+        googleMap.setOnCameraMoveListener(() -> marker.setPosition(this.googleMap.getCameraPosition().target));
+        googleMap.setOnCameraIdleListener(this::getLocation);
+    }
+
+    private void getLocation() {
+        LatLng currentLocation = this.googleMap.getCameraPosition().target;
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            addresses = geocoder.getFromLocation(currentLocation.latitude, currentLocation.longitude, 5); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0);
+            Log.i("kamarul", address);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+//        String city = addresses.get(0).getLocality();
+//        String state = addresses.get(0).getAdminArea();
+//        String country = addresses.get(0).getCountryName();
+//        String postalCode = addresses.get(0).getPostalCode();
+//        String knownName = addresses.get(0).getFeatureName();
     }
 }
